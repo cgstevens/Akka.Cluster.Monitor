94e84ce50aafc2014449ea898bdfb66f11d6d47e
Chris G. Stevens <cg.stevens@outlook.com>
2016-01-24 09:54:56 -0500

    Made all of the chtml pages look the same.... just plain white.  Need some UI to make it pretty :) Added .bat files to make it easy to install and uninstall

---
+++ Help/ServicesInstall.bat
@@ -0,0 +1,9 @@
+C:\Dev\GitHub\Akka.Cluster.Monitor\Lighthouse\bin\Debug\Lighthouse.exe install
+C:\Dev\GitHub\Akka.Cluster.Monitor\Lighthouse2\bin\Debug\Lighthouse2.exe install
+C:\Dev\GitHub\Akka.Cluster.Monitor\Tasker\bin\Debug\Tasker.exe install
+C:\Dev\GitHub\Akka.Cluster.Monitor\Worker\bin\Debug\Worker.exe install
+C:\Dev\GitHub\Akka.Cluster.Monitor\Worker2\bin\Debug\Worker2.exe install
+C:\Dev\GitHub\Akka.Cluster.Monitor\WorkerWithWebApi\bin\Debug\WorkerWithWebApi.exe install
+
+
+
